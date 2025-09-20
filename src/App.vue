0f2126dc0df4e68db696ac583dd784d79a652292
Louis Grant <ljsgrant@gmail.com>
2025-09-20 09:54:28 +0100

    WIP BodyToFollow module; uncomment header, add header content, style header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
 	<div id="app">
 		<div class="header">
-			<!-- <h1 class="title">Hello, Worlds</h1>
-			<h2 class="subtitle">A tiny spatial spirograph toy</h2> -->
+			<div class="logo">🪐</div>
+			<div class="title-wrapper">
+				<h1 class="title">Hello, Worlds <span>🚀</span></h1>
+				<h2 class="subtitle">A tiny planetary spirograph toy</h2>
+			</div>
+			<p class="disclaimer">
+				No physics were harmed in the making of this software
+			</p>
 		</div>
 		<div class="content">
-			<ThreeScene />
+			<ThreeScene :star="star" :planets="planets" />
+		</div>
+		<div class="controls">
+			<BodyToFollow :planets="planets" />
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
 	import ThreeScene from "./components/ThreeScene.vue"
-
+	import BodyToFollow from "./components/BodyToFollow.vue"
 	export default {
 		name: "App",
-		components: { ThreeScene },
+		components: { ThreeScene, BodyToFollow },
 		data() {
 			return {}
 		},
@@ -23,7 +32,7 @@
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 	#app {
 		display: flex;
 		flex-direction: column;
@@ -35,12 +44,32 @@
 
 	.header {
 		display: flex;
-		flex-direction: column;
-		margin-bottom: 15px;
+		flex-direction: row;
+		margin-bottom: 12px;
+		color: white;
+		align-items: center;
+		padding: 0 32px 0 8px;
 	}
 
 	.title {
 		font-size: 22px;
 		font-weight: bold;
+
+		span {
+			margin-left: 4px;
+		}
+	}
+
+	.logo {
+		font-size: 42px;
+		margin-right: 11px;
+	}
+
+	.disclaimer {
+		font-size: 10px;
+		margin-left: 12px;
+		font-style: italic;
+		margin-top: auto;
+		margin-left: auto;
 	}
 </style>
---
+++ src/components/BodyToFollow.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h1>Body to Follow</h1>
+		<select
+			name="planets"
+			id="planets"
+			v-model="bodyToFollow"
+			@change="handleBodyToFollowChange"
+		>
+			<option value="volvo" v-for="planet in planets" :key="planet.name">
+				{{ planet.name }}
+			</option>
+		</select>
+	</div>
+</template>
+
+<script lang="ts">
+	export default {
+		name: "BodyToFollow",
+		props: {
+			planets: {
+				type: Array as PropType<Planet[]>,
+				required: true,
+			},
+		},
+		data() {
+			return {
+				bodyToFollow: "",
+			}
+		},
+		methods: {
+			handleBodyToFollowChange(event: Event) {
+				this.$emit("body-to-follow", this.bodyToFollow)
+			},
+		},
+	}
+</script>
